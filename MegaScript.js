5bc63706e336f31bfaccdfff9177d7258ca25c78
Marlis6er <tpointfk@gmail.com>
2026-06-17 07:34:57 +0200

    Extract html template into separate method

--- MegaScript.js
+++ MegaScript.js
@@ -47,10 +47,8 @@ const LINKS = [
 		name: "HIGHSCORES",
 		altName: "Highscores",
 		link: "/Highscores",
-		path: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-ol" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5"/>
-  <path d="M1.713 11.865v-.474H2c.217 0 .363-.137.363-.317 0-.185-.158-.31-.361-.31-.223 0-.367.152-.373.31h-.59c.016-.467.373-.787.986-.787.588-.002.954.291.957.703a.595.595 0 0 1-.492.594v.033a.615.615 0 0 1 .569.631c.003.533-.502.8-1.051.8-.656 0-1-.37-1.008-.794h.582c.008.178.186.306.422.309.254 0 .424-.145.422-.35-.002-.195-.155-.348-.414-.348h-.3zm-.004-4.699h-.604v-.035c0-.408.295-.844.958-.844.583 0 .96.326.96.756 0 .389-.257.617-.476.848l-.537.572v.03h1.054V9H1.143v-.395l.957-.99c.138-.142.293-.304.293-.508 0-.18-.147-.32-.342-.32a.33.33 0 0 0-.342.338zM2.564 5h-.635V2.924h-.031l-.598.42v-.567l.629-.443h.635z"/>
-</svg>`,
+		path: `<path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5"/>
+  		<path d="M1.713 11.865v-.474H2c.217 0 .363-.137.363-.317 0-.185-.158-.31-.361-.31-.223 0-.367.152-.373.31h-.59c.016-.467.373-.787.986-.787.588-.002.954.291.957.703a.595.595 0 0 1-.492.594v.033a.615.615 0 0 1 .569.631c.003.533-.502.8-1.051.8-.656 0-1-.37-1.008-.794h.582c.008.178.186.306.422.309.254 0 .424-.145.422-.35-.002-.195-.155-.348-.414-.348h-.3zm-.004-4.699h-.604v-.035c0-.408.295-.844.958-.844.583 0 .96.326.96.756 0 .389-.257.617-.476.848l-.537.572v.03h1.054V9H1.143v-.395l.957-.99c.138-.142.293-.304.293-.508 0-.18-.147-.32-.342-.32a.33.33 0 0 0-.342.338zM2.564 5h-.635V2.924h-.031l-.598.42v-.567l.629-.443h.635z"/>`,
 		viewBox: "16"
 	}
 ];
@@ -101,7 +99,15 @@ class AddLinks {
 			const listItem = document.createElement("li");
 			listItem.className = "flex-fill"; // Ensures proper desktop spacing
  
-			listItem.innerHTML = `
+			listItem.innerHTML = this.svgTemplate(linkObj);
+ 
+			if (mobileMenu) mobileMenu.appendChild(listItem.cloneNode(true));
+			if (desktopMenu) desktopMenu.appendChild(listItem);
+		}
+	}
+
+	svgTemplate(linkObj) {
+		return `
 				<a class="nav-link d-flex flex-column align-items-center px-md-0 px-2 leftNavLink" href="${linkObj.link}">
 					<svg class="mb-1" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 ${linkObj.viewBox} ${linkObj.viewBox}">
 						${linkObj.path}
@@ -109,10 +115,6 @@ class AddLinks {
 					<span class="text-center">${linkObj.name}</span>
 				</a>
 			`;
- 
-			if (mobileMenu) mobileMenu.appendChild(listItem.cloneNode(true));
-			if (desktopMenu) desktopMenu.appendChild(listItem);
-		}
 	}
 }
  
@@ -237,7 +239,8 @@ class DPEnergyRefillReminder {
 	constructor() {}
 	inSupporter(url) {
 		let modalText = document.querySelector("#useRefillConfirm p.card-text.modal-bodyText");
-		observeDOM(modalText, e => {
+
+		const modalObserver = e => {
 			const textSplit = e[0].target.innerText.split(' ');
 			const option = textSplit[textSplit.length - 1];
 			if(option !== "Energy?") return;
@@ -246,7 +249,8 @@ class DPEnergyRefillReminder {
 				modalText.innerHTML += `<br><span class="text-warning">Your max energy isn't 200!</span>`;
 			else if(document.querySelector("#currentEnergy").innerText !== "0")
 				modalText.innerHTML += `<br><span class="text-danger">Your current energy isn't 0!</span>`;
-		});
+		};
+		observeDOM(modalText, modalObserver);
 	}
 }
  
