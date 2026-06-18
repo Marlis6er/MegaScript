aa4142c0d445477299ed123a20acd480e5988cbe
Marlis6er <tpointfk@gmail.com>
2026-06-18 18:42:11 +0200

    Remove k, m, b autofill from BetterMoneyInputs Already in the game now

--- BetterMoneyInputs.js
+++ BetterMoneyInputs.js
@@ -17,61 +17,54 @@ class BetterMoneyInputs {
 		else
 			input.addEventListener("autoNumeric:initialized", () => this._changeAutonumeric(input));
 	}
-	_listener(e, input, max) {
-		let elem = AutoNumeric.getAutoNumericElement(input);
+	_listener(e, input) {
+		const elem = AutoNumeric.getAutoNumericElement(input);
 		const curVal = parseFloat(elem.get().replaceAll(',', ""));
-		if (e.ctrlKey)
-			return;
-		if (e.key === 'k')
-			elem.set(Math.min(max, Math.floor(curVal * 1000)));
-		else if (e.key === 'm')
-			elem.set(Math.min(max, Math.floor(curVal * 1000000)));
-		else if (e.key === 'b')
-			elem.set(Math.min(max, Math.floor(curVal * 1000000000)));
-		else if (e.key === 'a')
+		const max = elem.getSettings().maximumValue;
+		if (e.ctrlKey) return;
+
+		if (e.key === 'a')
 			elem.set(max);
 		else if (e.key === 'h')
 			elem.set(Math.floor(max / 2));
 	}
 	listener(e, input) {
-		const max = parseInt(input.max.replaceAll(',', ""));
-		if (AutoNumeric.isManagedByAutoNumeric(input))
-			this._listener(e, input, max);
-
+		if (AutoNumeric.isManagedByAutoNumeric(input)) 
+			this._listener(e, input);
 		else
-			input.addEventListener("autoNumeric:initialized", () => this._listener(e, input, max));
+			input.addEventListener("autoNumeric:initialized", () => this._listener(e, input));
 	}
 	update(input) {
 		this.changeAutonumeric(input);
 		input.addEventListener("keydown", e => this.listener(e, input));
 	}
 	inBank(url) {
-		let depositInput = document.querySelector("input#depositInput");
-		let withdrawInput = document.querySelector("input#withdrawInput");
+		const depositInput = document.querySelector("input#depositInput");
+		const withdrawInput = document.querySelector("input#withdrawInput");
 		if (depositInput === null) return;
 
 		this.update(depositInput);
 		this.update(withdrawInput);
 	}
 	inCartelArmory(url) {
-		let pointsDeposit = document.querySelector("input#pointsdepositquantity");
+		const pointsDeposit = document.querySelector("input#pointsdepositquantity");
 		if (pointsDeposit === null) return;
 
 		this.update(pointsDeposit);
 	}
 	inTradeView(url) {
-		let cashInput = document.querySelector("input#cashModifier");
-		let pointsInput = document.querySelector("input#pointsModifier");
+		const cashInput = document.querySelector("input#cashModifier");
+		const pointsInput = document.querySelector("input#pointsModifier");
 		if (cashInput === null || cashInput.disabled) return;
 
 		this.update(cashInput);
 		this.update(pointsInput);
 	}
 	inMarket(url) {
-		let pricePer = document.querySelector("input#priceper");
-		let pointsPricePer = document.querySelector("input#pointspriceper");
-		let qty = document.querySelector("input#quantity");
-		let pointsQty = document.querySelector("input#pointsquantity");
+		const pricePer = document.querySelector("input#priceper");
+		const pointsPricePer = document.querySelector("input#pointspriceper");
+		const qty = document.querySelector("input#quantity");
+		const pointsQty = document.querySelector("input#pointsquantity");
 		if (pricePer === null) return;
 
 		this.update(pricePer);
@@ -81,17 +74,17 @@ class BetterMoneyInputs {
 
 		pointsPricePer.min = 0; // Make it like pricePer
 		if (AutoNumeric.isManagedByAutoNumeric(pointsPricePer)) {
-			let elem = AutoNumeric.getAutoNumericElement(pointsPricePer);
+			const elem = AutoNumeric.getAutoNumericElement(pointsPricePer);
 			elem.update({ minimumValue: 0 });
-		}
-		else
+		} else {
 			pointsPricePer.addEventListener("autoNumeric:initialized", () => {
-				let elem = AutoNumeric.getAutoNumericElement(pointsPricePer);
+				const elem = AutoNumeric.getAutoNumericElement(pointsPricePer);
 				elem.update({ minimumValue: 0 });
 			});
+		}
 	}
 	inUserProfile(url) {
-		let sendCash = document.querySelector("input#sendCashInput");
+		const sendCash = document.querySelector("input#sendCashInput");
 		if (sendCash === null) return;
 
 		this.update(sendCash);
