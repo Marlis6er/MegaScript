9c8dd5c70fb4dd4d6e84012272825b84f0e44a44
Marlis <105355300+Marlis6er@users.noreply.github.com>
2026-07-24 06:59:37 +0200

    Add settings

--- ScriptSettings.js
+++ ScriptSettings.js
@@ -4,14 +4,14 @@ class ScriptSettings {
 		this.fullName = "Megascript Settings";
 	}
 	inSettings(URL) {
-		let navTabs = document.querySelector("#settingsNav .nav-tabs");
-		let tabContent = document.querySelector("#settingsNav .tab-content");
+		const navTabs = document.querySelector("#settingsNav .nav-tabs");
+		const tabContent = document.querySelector("#settingsNav .tab-content");
 		if (navTabs === null || tabContent === null) return;
 
 		const urlParams = new URLSearchParams(window.location.search);
 		const selected = urlParams.get("t") === this.name;
 
-		let button = document.createElement("button");
+		const button = document.createElement("button");
 		button.id = `v-tab-${this.name}`;
 		button.classList.add("nav-link", "settings-nav-link");
 		if (selected) button.classList.add("active");
@@ -34,7 +34,62 @@ class ScriptSettings {
 		tab.id = `v-content-${this.name}`;
 		tab.setAttribute("role", "tabpanel");
 		tab.setAttribute("aria-labelledby", `v-tab-${this.name}`);
-		tab.innerHTML = `<div class="card"><div class="card-body"> <h5 class="h5">${this.fullName}</h5><p class="card-text">Currently there's no toggleable script settings.</p></div></div>`;
+		tab.innerHTML = this._createSettingsTemplate();
+
+		const accordion = tab.querySelector('#classAccordion');
+		accordion.innerHTML += this._createSectionTemplate('accordion-1', 'Bank Deposit Tax', 'Display the bank deposit tax', 'TestContent');
+		accordion.innerHTML += this._createSectionTemplate('accordion-2', 'Better Item Value', 'Display item value based on market prices', 'TestContent');
+		accordion.innerHTML += this._createSectionTemplate('accordion-3', 'Add Links', 'Add various extra links to the nav bar', 'TestContent');
+		accordion.innerHTML += this._createSectionTemplate('accordion-4', 'Better Money Inputs', 'Allow "h" to input half the money on hand', 'TestContent');
+		accordion.innerHTML += this._createSectionTemplate('accordion-5', 'Better Progress Bars', 'Fancy animated and striped progress bars', 'TestContent');
+		accordion.innerHTML += this._createSectionTemplate('accordion-6', 'Blackjack Helper', 'Highlight the optimal move', 'TestContent');
+		
+
 		tabContent.appendChild(tab);
+	}
+	_createSettingsTemplate() {
+		return `
+			<div class="card-border-0">
+				<div class="card-body">
+					<div class="d-flex flex-column align-items-center">
+						<div class="container classWrapper">
+							<div class="accordion accordion-flush" id="classAccordion">
+								<!-- Sections here -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`;
+	}
+	_createSectionTemplate(id, name, description, content) {
+		return `
+		<div class="accordion-item">
+			${this._createSectionHeaderTemplate(id, name, description)}
+			${this._createSectionBodyTemplate(id, content)}
+			<!-- Section header here -->
+			<!-- Section body here -->
+		</div>
+		`;
+	}
+	_createSectionHeaderTemplate(id, name, description) {
+		return `
+			<h2 class="accordion-header" id="heading-${id}">
+				<button class="accordion-button bg-dark rounded collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${id}" aria-expanded="false" aria-controls="${id}">
+					<div class="d-none d-sm-inline">
+						<span class="text-center">${name} - ${description}</span>
+					</div>
+				</button>
+			</h2>
+		`;
+	}
+	_createSectionBodyTemplate(id, content) {
+		return `
+			<div class="accordion-collapse collapse" id="${id}" aria-labelledby="heading-${id}" data-bs-parent="#classAccordion">
+				<div class="accordion-body">
+					${content}
+				</div>
+			</div>
+		`;
 	}
 }
