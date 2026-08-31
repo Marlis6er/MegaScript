8b499ab5a46882db1e10a12dc21bfb1a52871f74
Marlis <105355300+Marlis6er@users.noreply.github.com>
2026-08-31 15:02:27 +0200

    Add check to ensure all perks are visible

--- DisplayPerks.js
+++ DisplayPerks.js
@@ -87,6 +87,10 @@ Hospital timer by ${hospTimeText} ${this.getPerk(this.medEffectiveness) === 0 ? 
 		return result;
 	}
 	inHomepage(url) {
+		// Ensure that all perks are visible
+		const perkSelect = document.querySelector('#perkCategoryFilter');
+		if (perkSelect?.selectedOptions?.value !== 'all') return;
+		
 		// Grab the perk items from the updated structure
 		const perks = document.querySelectorAll(".col-12.d-flex.align-items-stretch.col-xxl-4 .perk-item");
 		if (perks?.length <= 0) return;
